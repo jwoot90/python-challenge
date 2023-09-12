0d772b057462eabafc2738e696f31ffdaf441ce4
jwoot90 <35399563+jwoot90@users.noreply.github.com>
2023-09-11 23:06:25 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,75 +1,44 @@
 import os
 import csv
-import pandas as pd 
-#import numpy as np
+import pandas as pd
 
-month_col = []
-pl_total_col = []
-change_in_pl = []
-period_one = []
-period_two = []
-greatest_inc = []
-count = 0
 
-df = pd.read_csv("C:/Users/jwoot/Documents/GWU-VIRT-DATA-PT-08-2023-U-LOLC/python-challenge/PyBank/resources/budget_data.csv")
-with open("C:/Users/jwoot/Documents/GWU-VIRT-DATA-PT-08-2023-U-LOLC/python-challenge/PyBank/resources/budget_data.csv") as csvfile:
-    csvreader = csv.reader(csvfile,delimiter = ',')
-    #skip header
-    csv_header = next(csvfile)
-    for row in csvreader:
-        
-        
 
-        #tally months
-        month_col.append(row[0])
-        total_months = len(str(month_col))
+df = pd.read_csv("C:/Users/jwoot/Documents/GWU-VIRT-DATA-PT-08-2023-U-LOLC/python-challenge/PyPoll/resources/election_data.csv")
 
-        #sum all profits and losses
-         
-        pl_total_col.append(int(row[1]))
-        
+#tally otes
+total_votes = df['Ballot ID'].count()
+candidate_counts = df['Candidate'].value_counts()[['Charles Casper Stockham','Diana DeGette','Raymon Anthony Doane']]
+winner = df['Candidate'].value_counts()[['Charles Casper Stockham','Diana DeGette','Raymon Anthony Doane']].max()
 
-        
-        
-for col in pl_total_col:
-    count += 1
-    if count % 2 == 1: 
-        period_one.append(col)  
-    elif count % 2 == 0:
-        period_two.append(col)  
-            #change in profit and loss
-for col in range(len(period_one)):
-    change_in_pl.append(period_two[col]- period_one[col])
-        
-pl_total = sum(i for i in pl_total_col)
-pl_average = round(sum(change_in_pl)/len(change_in_pl),2)
 
-#greatest increase and decrease
-greatest_inc = df.loc[df['Profit/Losses'] == df['Profit/Losses'].max()]
-greatest_dec = df[df['Profit/Losses'] == df['Profit/Losses'].min()]
+#Get candidate counts
+charles_count = df['Candidate'].value_counts()[['Charles Casper Stockham']]
+diana_count = df['Candidate'].value_counts()[['Diana DeGette']]
+raymon_count = df['Candidate'].value_counts()[['Raymon Anthony Doane']]
+
+#get candidate percentages
+charles_percentage= round(df['Candidate'].value_counts()['Charles Casper Stockham']/total_votes,3) * 100 
+diana_percentage = round(df['Candidate'].value_counts()['Diana DeGette']/total_votes,3) * 100
+raymon_percentage = round(df['Candidate'].value_counts()['Raymon Anthony Doane']/total_votes,3) * 100 
+
+#create variables for rows which will be passed to output file
+total_row = f'Total Votes: {total_votes} ' + "\n"
+diana_row = f'Diana DeGette: {diana_percentage}' + "%" +" " + f'{diana_count[0]}' + "\n"
+charles_row = f'Charles Casper Stockham: {charles_percentage}' + "%" + " " + f'{charles_count[0]}'+ "\n"
+raymon_row = f'Raymon Anthony Doane: {raymon_percentage}' + "%" + " " + f'{raymon_count[0]}'+ "\n"
+winner_row = f'Winner:{winner}' 
 
 
 
 
 
-total_month_row = f'Total Months: {total_months}' + "\n"
-pl_total_row = f'Total: {pl_total}' + "\n"
-average_change_row = f'Average Change: {pl_average}' + "\n"      
-greatest_increase_row = f'Geatest Increase: {greatest_inc}'  + "\n"
-greatest_decrease_row = f'Greatest Decrease: {greatest_dec}' + "\n"
-
-#content_total_months = str(total_months)
-#content_pl_total = str(pl_total)
-#content_pl_average = str(pl_average)
-#content_greatest_inc = str(greatest_inc)
-#content_greatest_dec = str(greatest_dec)
-
-with open("pybank.txt","w",encoding = 'UTF-8') as txtfile:
-    txtfile.write('Financial Analysis' + "\n")
-    txtfile.write('------------------------------' + "\n")
-    txtfile.write(total_month_row)
-    txtfile.write(pl_total_row)
-    txtfile.write(average_change_row)
-    txtfile.write(greatest_increase_row)
-    txtfile.write(greatest_decrease_row)
-
+#set up data for output file
+with open("election_data.txt","w",encoding = 'UTF-8') as txtfile:
+  txtfile.write("Election Results " + "\n")
+  txtfile.write(total_row)
+  txtfile.write(diana_row)
+  txtfile.write(charles_row)
+  txtfile.write(raymon_row)
+  txtfile.write(winner_row)
+  
